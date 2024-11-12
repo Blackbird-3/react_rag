df6634a86aaa0de9d6137619333e71186df23de5
PratyutCS <pratyut987@gmail.com>
2024-11-12 19:44:03 +0530

    test 1 check

--- rag_backend/server.py
+++ rag_backend/server.py
@@ -15,29 +15,32 @@ def server_status():
 
 @app.route("/answer" , methods = ['POST'])
 def answer():
-    os.environ["PINECONE_API_KEY"] = "pcsk_3JC6xT_2xm7TneUZK8EiVtVbRmUkuaZDTNJdswxNLQEZkhNViZXoU79T5JHwgKzS4fmkKn"
-    os.environ["GROQ_API_KEY"]= "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"
-    data= request.json
-    index_name = "rag-fullstack"
-    query = data.get('query')
-    print(query)
-    llm=ChatGroq(
-        groq_api_key=os.environ["GROQ_API_KEY"],
-        model_name="llama-3.1-8b-instant",
-        temperature=0.5
-    )
-    knowledge = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=OllamaEmbeddings(model="llama3.1"))
-    qa= RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=knowledge.as_retriever()
-    )
-    res = "I am Empty"
     try:
-        res = qa.invoke(query).get("result")
-    except Exception as error:
-        res = "error in getting data : "+error
-    return jsonify({"answer": res})
+        os.environ["PINECONE_API_KEY"] = "pcsk_3JC6xT_2xm7TneUZK8EiVtVbRmUkuaZDTNJdswxNLQEZkhNViZXoU79T5JHwgKzS4fmkKn"
+        os.environ["GROQ_API_KEY"]= "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"
+        data= request.json
+        index_name = "rag-fullstack"
+        query = data.get('query')
+        print(query)
+        llm=ChatGroq(
+            groq_api_key=os.environ["GROQ_API_KEY"],
+            model_name="llama-3.1-8b-instant",
+            temperature=0.5
+        )
+        knowledge = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=OllamaEmbeddings(model="llama3.1"))
+        qa= RetrievalQA.from_chain_type(
+            llm=llm,
+            chain_type="stuff",
+            retriever=knowledge.as_retriever()
+        )
+        res = "I am Empty"
+        try:
+            res = qa.invoke(query).get("result")
+        except Exception as error:
+            res = "error occured at chk1"
+        return jsonify({"answer": res})
+    except:
+        return jsonify({"answer": "error occured at chk2"})
     # return jsonify({"answer": qa.invoke(query).get("result")})
 
 if __name__ == "__main__":
