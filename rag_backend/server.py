36a41f0d929d7ee1fb08adc83f35880ac229e65f
Shreshtth Kumar Agarwaal <agarwalshreshth3@gmail.com>
2024-11-12 15:53:47 +0530

    trying new port

--- rag_backend/server.py
+++ rag_backend/server.py
@@ -36,5 +36,5 @@ def answer():
 
 if __name__ == "__main__":
     # app.run(debug=True , port = 10000)   
-    port = int(os.environ.get("PORT", 10000)) 
+    port = int(os.environ.get("PORT", 11434 )) 
     app.run(debug=True,host='0.0.0.0', port=port)
