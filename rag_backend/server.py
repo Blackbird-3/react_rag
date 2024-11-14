5f5f0b537265d40ea737eb32c06ea08087faccc5
Shreshtth Kumar Agarwaal <92109396+Blackbird-3@users.noreply.github.com>
2024-11-14 23:19:10 +0530

    changed embeddings

--- rag_backend/server.py
+++ rag_backend/server.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import os
 from langchain.chains import RetrievalQA
 from langchain_groq import ChatGroq
-from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_pinecone import PineconeVectorStore
 
 app = Flask(__name__)
@@ -15,42 +15,27 @@ def server_status():
 
 @app.route("/answer" , methods = ['POST'])
 def answer():
-    try:
-        os.environ["PINECONE_API_KEY"] = "pcsk_3JC6xT_2xm7TneUZK8EiVtVbRmUkuaZDTNJdswxNLQEZkhNViZXoU79T5JHwgKzS4fmkKn"
-        os.environ["GROQ_API_KEY"]= "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"
-        data= request.json
-        index_name = "rag-fullstack"
-        query = data.get('query')
-        print(query)
-        llm=ChatGroq(
-            groq_api_key=os.environ["GROQ_API_KEY"],
-            model_name="llama-3.1-8b-instant",
-            temperature=0.5
-        )
-        knowledge = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=OllamaEmbeddings(model="llama3.1"))
-        qa= RetrievalQA.from_chain_type(
-            llm=llm,
-            chain_type="stuff",
-            retriever=knowledge.as_retriever()
-        )
-        res = ""
-        try:
-            res = qa.invoke(query).get("result")
-        except Exception as e:
-            print("e1 : {}".format(e))
-            res = res+"error occured at chk1"
-        try:
-            res = qa.run(query)
-        except Exception as e:
-            print("e2 : {}".format(e))
-            res = res+" -- error occured at chk2"
-        return jsonify({"query":query,"answer": res})
-    except Exception as e:
-        print("e2 : {}".format(e))
-        return jsonify({"query":query,"answer": "error occured at chk3"})
-    # return jsonify({"answer": qa.invoke(query).get("result")})
+    os.environ["PINECONE_API_KEY"] = "pcsk_3JC6xT_2xm7TneUZK8EiVtVbRmUkuaZDTNJdswxNLQEZkhNViZXoU79T5JHwgKzS4fmkKn"
+    os.environ["GROQ_API_KEY"]= "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"
+    data= request.json
+    index_name = "rag-huggingface"
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    query = data.get('query')
+    print(query)
+    llm=ChatGroq(
+        groq_api_key=os.environ["GROQ_API_KEY"],
+        model_name="llama-3.1-8b-instant",
+        temperature=0.5
+    )
+    knowledge = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=embeddings)
+    qa= RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=knowledge.as_retriever()
+    )
+    return jsonify({"answer": qa.invoke(query).get("result")})
 
 if __name__ == "__main__":
-    # app.run(debug=True , port = 10000)   
-    port = int(os.environ.get("PORT", 3000 )) 
+    # app.run(debug=True , port = 5001)   
+    port = int(os.environ.get("PORT", 10000 )) 
     app.run(debug=True,host='0.0.0.0', port=port)
